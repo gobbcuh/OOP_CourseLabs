89dd9f469a787b41fa6137a82a7ff0f50986c3a1
gobbcuh <jajalorino@gmail.com>
2025-04-06 21:48:26 +0800

    done encap

--- src/encapsulation_sdpt/Main.java
+++ src/encapsulation_sdpt/Main.java
@@ -2,6 +2,17 @@ package encapsulation_sdpt;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        User u = new User(0, null, null, null);
+
+        u.setUserID(488);
+        u.setUsername("gobbcuh");
+        u.setFirstName("Pandesal");
+        u.setLastName("Mocha");
+
+        System.out.println("User ID: " + u.getUserID());
+        System.out.println("Username: " + u.getUsername());
+        System.out.println("First Name: " + u.getFirstName());
+        System.out.println("Last Name: " + u.getLastName());
     }
 }
--- src/encapsulation_sdpt/User.java
+++ src/encapsulation_sdpt/User.java
@@ -2,4 +2,45 @@ package encapsulation_sdpt;
 
 public class User {
     
+    private int userID;
+    private String username, firstName, lastName;
+
+    User (int userID, String username, String firstName, String lastName) {
+        this.userID = userID;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    void setUsername(String username) {
+        this.username = username;
+    }
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    int getUserID() {
+        return userID;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
 }
