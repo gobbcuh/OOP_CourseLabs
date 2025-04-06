2c0ad7fe83268a91c9ced95d1308e46301f73538
gobbcuh <jajalorino@gmail.com>
2025-04-06 21:25:51 +0800

    added sdpt encap code

---
+++ src/encapsulation_sdpt/Main.java
@@ -0,0 +1,7 @@
+package encapsulation_sdpt;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/encapsulation_sdpt/User.java
@@ -0,0 +1,5 @@
+package encapsulation_sdpt;
+
+public class User {
+    
+}
