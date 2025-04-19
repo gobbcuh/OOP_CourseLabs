8920290326ec6c84659cc0c94bc2349075354b80
gobbcuh <jajalorino@gmail.com>
2025-04-19 08:22:21 +0800

    overriden methods

--- src/laboratory4_2/Triangle.java
+++ src/laboratory4_2/Triangle.java
@@ -1,5 +1,12 @@
 package laboratory4_2;
 
 public class Triangle extends Shape {
-    
+    @Override
+    void calculateArea() {
+        System.out.println("Calculating area of Circle");
+    }
+    @Override
+    void calculatePerimeter() {
+        System.out.println("Calculating perimeter of Circle");
+    }
 }
